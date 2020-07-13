d8789767cfff90227ab247e592641a4c63b67be1
Sagnik <62300323+Wicked-North@users.noreply.github.com>
2020-07-14 00:26:28 +0530

    Add files via upload

---
+++ map/HERE maps api/test.js
@@ -0,0 +1,21 @@
+var citymap = [
+    
+     { lat: 41.878, lng: -87.629 },
+   
+  
+    { lat: 40.714, lng: -74.005 },
+  
+  
+   
+
+     { lat: 49.25, lng: -123.1 },
+    { lat: 34.052, lng: -118.243 }
+ 
+];
+
+for (var center in citymap) {
+
+    console.log(citymap[center]);
+}
+    
+         
