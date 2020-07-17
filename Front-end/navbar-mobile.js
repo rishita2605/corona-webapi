f9fb7cb338bc19dbac6cad6f57aa3f6a70522b38
rishita2605 <64982040+rishita2605@users.noreply.github.com>
2020-07-17 15:29:47 +0530

    dark/light icon

--- Front-end/navbar-mobile.js
+++ Front-end/navbar-mobile.js
@@ -1,9 +1,12 @@
-//variables + events
+//variables + eventListener
+$(".mode").on('click',modeChange);
+var mo=$(".mode");
+var icon=$(".mode-icon");
 
 
-//event listener
 
 //functions
+//nav-bar fluid motion
 function move(id, position) {
 
     var tl = gsap.timeline();
@@ -33,3 +36,48 @@ function move(id, position) {
 }
 
 
+//dark mode-light mode switching
+function modeChange(){
+  
+  console.log(mo.css("background-color"));
+  if(mo.css("background-color")=="rgb(0, 163, 255)"){
+    icon.css("transform","scale(0.1)");
+    //mo.css("transform","scale(0.1)");
+    setTimeout(light,200);
+    
+  }
+  else{
+    icon.css("transform","scale(0.1)");
+    //mo.css("transform","scale(0.1)");
+    setTimeout(dark,200);
+  }
+}
+
+function light(){
+    console.log("light");
+    console.log(icon);
+    icon.css("transform","scale(1)");
+    //mo.css("transform","scale(1)");
+
+    mo.css("background-color","rgba(17, 29, 67, 1)");
+    mo.css("color","white");
+    icon.removeClass("fas fa-moon");
+    icon.addClass("fas fa-sun");
+    //icon transition end
+
+    //layout transition start
+}
+
+function dark(){
+    console.log("dark");
+    console.log(icon);
+    icon.css("transform","scale(1)");
+    //mo.css("transform","scale(1)");
+    mo.css("background-color","rgba(0, 163, 255, 1)");
+    mo.css("color","rgba(12, 18, 44, 1)");
+    icon.removeClass("fas fa-sun");
+    icon.addClass("fas fa-moon");
+    //icon transition end
+
+    //layout transition start
+}
